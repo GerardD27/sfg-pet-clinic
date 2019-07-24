ed605f19976695c25d927203915ad24c820b0875
Gerard <Debold.gerard@gmail.com>
2019-07-24 11:44:23 -0400

    Refactored project to be multi module, split into pet-clinic-data and pet-clinic-web

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,5 +1,4 @@
-package gerard.springframework.sfgpetclinic;
-
+package sfgpetclinic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
